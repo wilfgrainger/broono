4c42b1c5c5bc4524dca41a1fb3a6bdcd1b3a507a
wilfgrainger <51911955+wilfgrainger@users.noreply.github.com>
2026-08-02 05:33:52 +0100

    Load final responsive product polish

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import './polish.css'
 
 const RESTORE_SESSION_KEY = 'broono:glp-runtime-reset:v3'
 
